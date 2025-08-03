7c3e4f08c0346e96dab96d009999376a76fe860d
Muhammad Ahmad Khan <muhammadahmaddd8@gmail.com>
2025-08-03 17:23:29 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/professional-cv-website/',
+  base: '',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
